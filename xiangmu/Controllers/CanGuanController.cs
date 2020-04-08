4ba59705ced06a53b0e86dbf33fb16036bef98a8
SW <SW@DESKTOP-BV2HV76>
2020-04-08 22:03:15 +0800

    CanGuan

---
+++ xiangmu/Controllers/CanGuanController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace xiangmu.Controllers
+{
+    public class CanGuanController : Controller
+    {
+        // GET: CanGuan
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult RuKu()
+        {
+
+            return View();
+        }
+        public ActionResult RuKuInsert()
+        {
+
+            return View();
+        }
+        public ActionResult ChuKu()
+        {
+            return View();
+
+        }
+
+        public ActionResult ChuKuInsert()
+        {
+            return View();
+        }
+
+        public ActionResult BaoSun()
+        {
+            return View();
+        }
+        public ActionResult BaoSunInsert()
+        {
+
+            return View();
+        }
+        public ActionResult YiKu()
+        {
+            return View();
+        }
+        public ActionResult PanDian()
+        {
+            return View();
+        }
+        public ActionResult TuiHuo()
+        {
+            return View();
+        }
+        public ActionResult a()
+        {
+            return View();
+        }
+    }
+}
