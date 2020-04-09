408be5f435500ee0b67ca9691bac4b21a9b4e1bb
1693215776@qq.com <1693215776@qq.com>
2020-04-09 16:05:11 +0800

    母版页

---
+++ xiangmu/Controllers/mubanyeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace xiangmu.Controllers
+{
+    public class mubanyeController : Controller
+    {
+        // GET: mubanye
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ym()
+        {
+            return View();
+        }
+
+
+    }
+}
